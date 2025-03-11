24e8fcd3208d6770cf156ce147a139f44b944325
jsv7 <jsv7@protonmail.com>
2025-03-11 19:54:11 +0300

    3

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import { Unity, useUnityContext } from "react-unity-webgl";
+
+
+function App() {
+  const  { unityProvider }  = useUnityContext({
+      loaderUrl: "/assets/WebGL.loader.js",
+      dataUrl: "/assets/WebGL.data.unityweb",
+      frameworkUrl: "/assets/WebGL.framework.js.unityweb",
+      codeUrl: "/assets/WebGL.wasm.unityweb",
+    });
+
+    return (
+      <div>
+      <h1>Hahaha </h1>
+        <Unity
+          style={{
+            width:"80%",
+
+            justifySelf: "center",
+            alignSelf: "center",
+          }}
+          unityProvider = {unityProvider}
+        />
+      </div>
+    );
+}
+
+export default App;
