dcc8711d148e60cdc753c5aa8bc5d5c444fe633e
jsv7 <jsv7@protonmail.com>
2025-03-13 18:57:40 +0300

    10

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
+
+function App() {
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "Build/WebGL.loader.js",
+    dataUrl: "Build/WebGL.data.unityweb",
+    frameworkUrl: "Build/WebGL.framework.js.unityweb",
+    codeUrl: "Build/WebGL.wasm.unityweb",
+  });
+
+  return <Unity unityProvider={unityProvider} />;
+}
