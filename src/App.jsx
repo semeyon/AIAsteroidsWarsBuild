6df7b11bbe5f659d6bde45b168a462e2f530064d
jsv7 <jsv7@protonmail.com>
2025-03-14 09:24:05 +0300

    12

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 function App() {
-  const { unityProvider } = useUnityContext({
+  const { unityProvider, sendMessage } = useUnityContext({
     loaderUrl: "Build/WebGL.loader.js",
     dataUrl: "Build/WebGL.data.unityweb",
     frameworkUrl: "Build/WebGL.framework.js.unityweb",
     codeUrl: "Build/WebGL.wasm.unityweb",
   });
 
-  return <Unity unityProvider={unityProvider} />;
+  function handleClickSpawnEnemies() {
+    sendMessage("TESTtext", "SpawnEnemies", 100);
+  }
+
+  return (
+    <Fragment>
+      <Unity unityProvider={unityProvider} />
+      <button onClick={handleClickSpawnEnemies}>Spawn Enemies</button>
+    </Fragment>
+
 }
