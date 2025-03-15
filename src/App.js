a80277a8192971dee99a6acf697f0d6cb066c262
jsv7 <jsv7@protonmail.com>
2025-03-15 11:08:24 +0300

    12

--- src/App.js
+++ src/App.js
@@ -1,47 +1,15 @@
 import './App.css';
-//import React, { Fragment, useCallback, useEffect } from "react";
-import React, { Fragment } from "react";
+import React from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
-//import { hapticFeedback } from '@telegram-apps/sdk';
 
 function App() {
-  const { unityProvider/*, addEventListener, removeEventListener*/ } = useUnityContext({
+  const {unityProvider} = useUnityContext({
     loaderUrl: "/assets/WebGL.loader.js",
     dataUrl: "/assets/WebGL.data.unityweb",
     frameworkUrl: "/assets/WebGL.framework.js.unityweb",
     codeUrl: "/assets/WebGL.wasm.unityweb",
   });
-  // function haptic() {
-  //   if (window.Telegram.WebApp) {
-  //
-  //     const haptic = window.Telegram.WebApp.HapticFeedback;
-  //
-  //   // Trigger haptic feedback on a click event
-  //
-  //     document.querySelector('#clickerButton').addEventListener('click', function() {
-  //     haptic.impactOccurred('light');
-  //
-  //          // Other game logic
-  //
-  //      });
-  //
-  //    }
-  // }
-  // const handleHaptic = useCallback((userName, score) => {
-  //   setIsGameOver(true);
-  //   setUserName(userName);
-  //   setScore(score);
-  // }, []);
-  //
-  // useEffect(() => {
-  //   addEventListener("GameOver", handleHaptic);
-  //   return () => {
-  //     removeEventListener("GameOver", handleHaptic);
-  //   };
-  // }, [addEventListener, removeEventListener, handleHaptic]);
-
   return (
-    <Fragment>
       <Unity
         style ={{
           width: "80%",
@@ -51,8 +19,7 @@ function App() {
         }}
         unityProvider={unityProvider}
       />
-      <button /*onClick={haptic}*/>Haptic</button>
-    </Fragment>
+      <h1>Haptic</h1>
   );
 }
 export default App;
