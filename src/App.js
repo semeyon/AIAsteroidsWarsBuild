64e716e51614baf43754fb12a1b1b1491f1700e4
jsv7 <jsv7@protonmail.com>
2025-03-15 13:40:50 +0300

    12

--- src/App.js
+++ src/App.js
@@ -1,57 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import React, { Fragment, useCallback, useEffect } from "react";
-import { Unity, useUnityContext } from "react-unity-webgl";
-import { hapticFeedback } from '@telegram-apps/sdk';
 
 function App() {
-  const { unityProvider, addEventListener, removeEventListener } = useUnityContext({
-    loaderUrl: "/assets/WebGL.loader.js",
-    dataUrl: "/assets/WebGL.data",
-    frameworkUrl: "/assets/WebGL.framework.js",
-    codeUrl: "/assets/WebGL.wasm",
-  });
-  function haptic() {
-    if (window.Telegram.WebApp) {
-
-      const haptic = window.Telegram.WebApp.HapticFeedback;
-
-    // Trigger haptic feedback on a click event
-
-      document.querySelector('#clickerButton').addEventListener('click', function() {
-      haptic.impactOccurred('light');
-
-           // Other game logic
-
-       });
-
-     }
-  }
-  // const handleHaptic = useCallback((userName, score) => {
-  //   setIsGameOver(true);
-  //   setUserName(userName);
-  //   setScore(score);
-  // }, []);
-  //
-  // useEffect(() => {
-  //   addEventListener("GameOver", handleHaptic);
-  //   return () => {
-  //     removeEventListener("GameOver", handleHaptic);
-  //   };
-  // }, [addEventListener, removeEventListener, handleHaptic]);
-
   return (
-    <Fragment>
-      <Unity
-        style ={{
-          width: "80%",
-
-          justifySelf: "center",
-          alignSelf: "center",
-        }}
-        unityProvider={unityProvider}
-      />
-      <button onClick={haptic}>Haptic</button>
-    </Fragment>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
 export default App;
